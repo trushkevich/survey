e83bbe59ab04e7d85bb91925690933687a970278
Anton Trushkevich <trushkevich@gmail.com>
2014-07-01 13:23:52 +0300

    added module wrapping to models

--- app/models/survey/answer.rb
+++ app/models/survey/answer.rb
@@ -1,36 +1,38 @@
-class Answer < ::ActiveRecord::Base
-  self.table_name = "survey_answers"
+module Survey
+  class Answer < ::ActiveRecord::Base
+    self.table_name = "survey_answers"
 
-  acceptable_attributes :attempt, :option, :correct, :option_id, :question, :question_id, :text
+    acceptable_attributes :attempt, :option, :correct, :option_id, :question, :question_id, :text
 
-  # associations
-  belongs_to :attempt
-  belongs_to :option
-  belongs_to :question
+    # associations
+    belongs_to :attempt
+    belongs_to :option
+    belongs_to :question
 
-  # validations
-  validates :option_id, :question_id, :presence => true
-  validates :option_id, :uniqueness => { :scope => [:attempt_id, :question_id] }
+    # validations
+    validates :option_id, :question_id, :presence => true
+    validates :option_id, :uniqueness => { :scope => [:attempt_id, :question_id] }
 
-  # callbacks
-  after_create :characterize_answer
+    # callbacks
+    after_create :characterize_answer
 
-  def value
-    if option_id > 0
-      points = (self.option.nil? ? ::Survey::Option.find(option_id) : self.option).weight
-      correct?? points : - points
-    else
-      0
+    def value
+      if option_id > 0
+        points = (self.option.nil? ? ::Survey::Option.find(option_id) : self.option).weight
+        correct?? points : - points
+      else
+        0
+      end
     end
-  end
 
-  def correct?
-    self.correct ||= self.option.try(:correct?)
-  end
+    def correct?
+      self.correct ||= self.option.try(:correct?)
+    end
 
-  private
+    private
 
-  def characterize_answer
-    update_attribute(:correct, option.try(:correct?))
+    def characterize_answer
+      update_attribute(:correct, option.try(:correct?))
+    end
   end
 end
--- app/models/survey/question.rb
+++ app/models/survey/question.rb
@@ -1,24 +1,26 @@
-class Survey::Question < ActiveRecord::Base
+module Survey
+  class Survey::Question < ActiveRecord::Base
 
-  self.table_name = "survey_questions"
+    self.table_name = "survey_questions"
 
-  acceptable_attributes :text, :survey, :options_attributes => Survey::Option::AccessibleAttributes
+    acceptable_attributes :text, :survey, :options_attributes => Survey::Option::AccessibleAttributes
 
-  # relations
-  belongs_to :survey
-  has_many   :options, :dependent => :destroy
-  accepts_nested_attributes_for :options,
-    :reject_if => ->(a) { a[:text].blank? },
-    :allow_destroy => true
+    # relations
+    belongs_to :survey
+    has_many   :options, :dependent => :destroy
+    accepts_nested_attributes_for :options,
+      :reject_if => ->(a) { a[:text].blank? },
+      :allow_destroy => true
 
-  # validations
-  validates :text, :presence => true, :allow_blank => false
+    # validations
+    validates :text, :presence => true, :allow_blank => false
 
-  def correct_options
-    return options.correct
-  end
+    def correct_options
+      return options.correct
+    end
 
-  def incorrect_options
-    return options.incorrect
+    def incorrect_options
+      return options.incorrect
+    end
   end
 end
